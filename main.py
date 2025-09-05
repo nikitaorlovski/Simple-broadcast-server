fc8610305eb4b66ce8f655ca5d663ea4e3bd53e0
Nikita Orlovsky <orlovski15555@gmail.com>
2025-09-05 23:24:42 +0300

    Fix bugs

--- main.py
+++ main.py
@@ -34,4 +34,4 @@ async def ws_endpoint(websocket: WebSocket, name: str = Query("Guest")):
         clients.remove(client)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=int(os.getenv("PORT", 8001)))
+    uvicorn.run("main:app", host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
